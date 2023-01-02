9f17f1c7d320e26f6a14d08e0c5a120443aaa2cb
AlexisCro <95683483+AlexisCro@users.noreply.github.com>
2023-01-02 13:55:41 +0100

    install fixer and test function

--- JS/fixer_API.js
+++ JS/fixer_API.js
@@ -1,13 +1,22 @@
-let myHeaders = new Headers();
-myHeaders.append("apikey", "g5abhnyuIjiV7oWQhZO6J7j7CJV3ac5h");
+let convertButton = document.getElementById('convert-button');
+let priceOutput  = document.getElementById('price-output');
 
-let requestOptions = {
-  method: 'GET',
-  redirect: 'follow',
-  headers: myHeaders
-};
-
-fetch("https://api.apilayer.com/fixer/convert?to={to}&from={from}&amount={amount}", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+convertButton.addEventListener('click', () => {
+    let priceInput   = document.getElementById('price-input').value;
+    let deviseInput  = document.getElementById('devise-input').value;
+    let deviseOutput = document.getElementById('devise-output').value;
+    let myHeaders    = new Headers();
+    
+    myHeaders.append("apikey", "g5abhnyuIjiV7oWQhZO6J7j7CJV3ac5h");
+        
+    let requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+        headers: myHeaders
+    };
+    
+    fetch(`https://api.apilayer.com/fixer/convert?to=${deviseOutput}&from=${deviseInput}&amount=${priceInput}`, requestOptions)
+        .then(response => response.json())
+        .then(result => priceOutput.value = result["result"])
+        .catch(error => console.log('error', error))
+})
