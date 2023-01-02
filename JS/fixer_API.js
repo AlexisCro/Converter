cf2084b500519c4119e60f78305a95d715b0dcea
AlexisCro <95683483+AlexisCro@users.noreply.github.com>
2023-01-02 17:16:22 +0100

    add a new API, add the dynamic list choice to choose the currencies in input and output

--- JS/fixer_API.js
+++ JS/fixer_API.js
@@ -1,5 +1,52 @@
+//manage fixer to have all currencies in the choice list
+let deviseInput  = document.getElementById('devise-input');
+let deviseOutput = document.getElementById('devise-output');
+let myHeaders = new Headers();
+myHeaders.append("apikey", "g5abhnyuIjiV7oWQhZO6J7j7CJV3ac5h");
+
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders
+};
+
+fetch("https://api.apilayer.com/exchangerates_data/symbols", requestOptions)
+  .then(response => response.json())
+  .then(result =>  {
+    let symbols = result.symbols;
+    for(i in symbols){
+        deviseInput.insertAdjacentHTML('afterbegin', `<option class="input-group-text" value=${i}>${symbols[i]}</option>`);
+        deviseOutput.insertAdjacentHTML('afterbegin', `<option class="input-group-text" value=${i}>${symbols[i]}</option>`);
+    }
+  })
+  .catch(error => console.log('error', error));
+
+// let myHeaders = new Headers();
+//myHeaders.append("apikey", "g5abhnyuIjiV7oWQhZO6J7j7CJV3ac5h");
+
+/*var requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders
+};*/
+
+/*fetch("https://api.apilayer.com/fixer/symbols", requestOptions)
+  .then(response => response.json())
+  .then(result => {
+    for(i in result.symbols){
+        deviseInput.insertAdjacentHTML('afterbegin', `<option class="input-group-text" value=${i}>${i}</option>`)
+    }
+  })
+  .catch(error => {
+    console.log('error', error)
+    if(error !== 'null'){
+        alert("Désolé, nous sommes victimes de notre succès et nous ne pouvons plus lancer de requête pour le moment. ");
+    }
+  });*/
+
+//manage the request to have the conversion 
 let convertButton = document.getElementById('convert-button');
-let priceOutput  = document.getElementById('price-output');
+let priceOutput   = document.getElementById('price-output');
 
 convertButton.addEventListener('click', () => {
     let priceInput   = document.getElementById('price-input').value;
@@ -7,7 +54,6 @@ convertButton.addEventListener('click', () => {
     let deviseOutput = document.getElementById('devise-output').value;
     let myHeaders    = new Headers();
     
-    myHeaders.append("apikey", "g5abhnyuIjiV7oWQhZO6J7j7CJV3ac5h");
         
     let requestOptions = {
         method: 'GET',
