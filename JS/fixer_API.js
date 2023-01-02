982cc38b798f33ce4edbd5afad6b1a30375e4139
AlexisCro <95683483+AlexisCro@users.noreply.github.com>
2023-01-02 17:17:56 +0100

    remove the fixer API to create the dynamic list choice

--- JS/fixer_API.js
+++ JS/fixer_API.js
@@ -21,29 +21,6 @@ fetch("https://api.apilayer.com/exchangerates_data/symbols", requestOptions)
   })
   .catch(error => console.log('error', error));
 
-// let myHeaders = new Headers();
-//myHeaders.append("apikey", "g5abhnyuIjiV7oWQhZO6J7j7CJV3ac5h");
-
-/*var requestOptions = {
-  method: 'GET',
-  redirect: 'follow',
-  headers: myHeaders
-};*/
-
-/*fetch("https://api.apilayer.com/fixer/symbols", requestOptions)
-  .then(response => response.json())
-  .then(result => {
-    for(i in result.symbols){
-        deviseInput.insertAdjacentHTML('afterbegin', `<option class="input-group-text" value=${i}>${i}</option>`)
-    }
-  })
-  .catch(error => {
-    console.log('error', error)
-    if(error !== 'null'){
-        alert("Désolé, nous sommes victimes de notre succès et nous ne pouvons plus lancer de requête pour le moment. ");
-    }
-  });*/
-
 //manage the request to have the conversion 
 let convertButton = document.getElementById('convert-button');
 let priceOutput   = document.getElementById('price-output');
@@ -64,5 +41,5 @@ convertButton.addEventListener('click', () => {
     fetch(`https://api.apilayer.com/fixer/convert?to=${deviseOutput}&from=${deviseInput}&amount=${priceInput}`, requestOptions)
         .then(response => response.json())
         .then(result => priceOutput.value = result["result"])
-        .catch(error => console.log('error', error))
+        .catch(error => alert('error', error))
 })
