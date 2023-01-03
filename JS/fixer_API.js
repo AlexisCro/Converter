24c0426154f8cbc00d36b199f8034becf6f7e303
AlexisCro <95683483+AlexisCro@users.noreply.github.com>
2023-01-03 11:55:37 +0100

    algo to balance between two API to have 250 request

--- JS/fixer_API.js
+++ JS/fixer_API.js
@@ -4,7 +4,7 @@ let deviseOutput = document.getElementById('devise-output');
 let myHeaders = new Headers();
 myHeaders.append("apikey", "g5abhnyuIjiV7oWQhZO6J7j7CJV3ac5h");
 
-var requestOptions = {
+let requestOptions = {
   method: 'GET',
   redirect: 'follow',
   headers: myHeaders
@@ -21,49 +21,42 @@ fetch("https://api.apilayer.com/exchangerates_data/symbols", requestOptions)
   })
   .catch(error => console.log('error', error));
 
-//manage the request to have the conversion 
+//manage the request to have the conversion
 let convertButton = document.getElementById('convert-button');
 let priceOutput   = document.getElementById('price-output');
 
 convertButton.addEventListener('click', () => {
-    let priceInput   = document.getElementById('price-input').value;
-    let deviseInput  = document.getElementById('devise-input').value;
-    let deviseOutput = document.getElementById('devise-output').value;    
-    let i            = 1;
-        
-    let requestOptions = {
-        method: 'GET',
-        redirect: 'follow',
-        headers: myHeaders
-    };
-    
-    while (i <= 2){
-      if (i === 1){
-        fetch(`https://api.apilayer.com/fixer/convert?to=${deviseOutput}&from=${deviseInput}&amount=${priceInput}`, requestOptions)
-          .then(response => response.json())
-          .then(result => {
-            console.log(result);
-            console.log(result.message);
-            if ( result.message !== null && result.message.includes("You have exceeded your daily/monthly API rate limit") ){
-              i ++;
-            } else {
-              priceOutput.value = result["result"];
-              i = 3;
-            }
-          })
-          .catch(error => alert('error', error));
-          console.log(`je passe ici est i = ${i}`);
-      }else if (i === 2){
-        fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${deviseOutput}&from=${deviseInput}&amount=${priceInput}`, requestOptions)
-          .then(response => response.json())
-          .then(result => {
-            if (result.message !== null){
-              alert(result.message);
-              i++;
-            } else {
-              priceOutput.value = result["result"];
-            }
-          })
-      }
-}
+  let priceInput   = document.getElementById('price-input').value;
+  let deviseInput  = document.getElementById('devise-input').value;
+  let deviseOutput = document.getElementById('devise-output').value; 
+
+  myHeaders = new Headers();
+  myHeaders.append("apikey", "g5abhnyuIjiV7oWQhZO6J7j7CJV3ac5h");
+
+  let requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+      headers: myHeaders
+  };
+
+  fetch(`https://api.apilayer.com/fixer/convert?to=${deviseOutput}&from=${deviseInput}&amount=${priceInput}`, requestOptions)
+    .then(response => response.json())
+    .then(result =>
+      { console.log(result.message);
+        if (result.message.includes("You have exceeded") ){
+          console.log("je suis dans la première condition");
+          fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${deviseOutput}&from=${deviseInput}&amount=${priceInput}`, requestOptions)
+            .then(response => response.json())
+            .then(result => {
+              if (Object.keys(result).includes("message")){
+                alert("Nous sommes désolés mais nous sommes victimes de notre succès");
+              } else {
+                priceOutput.value = result["result"];
+              }
+            })
+        } else {
+          priceOutput.value = result["result"];
+        }
+      })
+    .catch(error => console.log("error", error));
 })
