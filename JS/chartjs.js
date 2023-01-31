73548cd11ed0a06e4152c25b1d90b1e9a1aba16c
AlexisCro <95683483+AlexisCro@users.noreply.github.com>
2023-01-31 15:49:59 +0100

    strat manage chart

--- JS/chartjs.js
+++ JS/chartjs.js
@@ -1,5 +1,17 @@
-const myChart    = document.getElementById('myChart');
-const MONTHS     = {
+// Call API
+let myHeaders = new Headers();
+myHeaders.append("apikey", "g5abhnyuIjiV7oWQhZO6J7j7CJV3ac5h");
+
+let requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders
+};
+
+// script to construct and display charts
+const myChart  = document.getElementById('myChart');
+const BUTTON   = document.getElementById('rates-button');
+const MONTHS   = {
     1: 'JANVIER',
     2: 'FEVRIER',
     3: 'MARS',
@@ -24,7 +36,7 @@ function numberOfMonthBetweenTwoDates(date1, date2){
     return result;
 };
 
-function labels(date1, date2){
+function labels(date1, date2, startMonth){
     let array = [];
     let m = startMonth;
     for (let i = 0; i <= numberOfMonthBetweenTwoDates(date1, date2); i++){
@@ -40,21 +52,97 @@ function labels(date1, date2){
     return array;
 };
 
-
-// Ces variables sont à définir seulement au moment du clic sur bouton
-const startDate  = new Date(document.getElementById('start-date').value);
-const endDate    = new Date(document.getElementById('end-date').value);
-const startMonth = startDate.getMonth() + 1;
-const startYear  = startDate.getFullYear();
-const endMonth   = endDate.getMonth() + 1;
-const endYear    = endDate.getFullYear();
-
-const lineChart = new Chart(myChart, {
+// display default chart
+const exampleChart = new Chart(myChart, {
     type: 'line',
     data: {
-        labels: labels(startDate, endDate),
+        labels: Object.values(MONTHS),
         datasets: [{
-            data:[]
+            label: `devise`,
+            data: [],
+            borderColor: 'red',
+            backgroundColor: '#f8c0b4'
         }]
+    },
+    options: {
+        transitions: {
+            show: {
+                animations: {
+                    x: {
+                        from: 0
+                    },
+                    y: {
+                        from: 0
+                    }
+                }
+            },
+            hide: {
+                animations: {
+                    x: {
+                        to: 0
+                    },
+                    y: {
+                        to: 0
+                    }
+                }
+            }
+        }
     }
+})
+
+BUTTON.addEventListener('click', ()=>{
+    const startDate    = new Date(document.getElementById('start-date').value);
+    const startDateAPI = document.getElementById('start-date').value;
+    const endDateAPI   = document.getElementById('end-date').value
+    const endDate      = new Date(document.getElementById('end-date').value);
+    const startMonth   = startDate.getMonth() + 1;
+    const startYear    = startDate.getFullYear();
+    const endMonth     = endDate.getMonth() + 1;
+    const endYear      = endDate.getFullYear();
+    const DEVISE       = document.getElementById('devise-chart').value;
+
+    fetch(`https://api.apilayer.com/exchangerates_data/fluctuation?start_date=${startDateAPI}&end_date=${endDateAPI}&symbols=${DEVISE}`, requestOptions)
+    .then(response => response.json())
+    .then(result => {
+        let rates = result["rates"];
+        console.log(result);
+        exampleChart.destroy();
+        const realChart = new Chart(myChart, {
+        type: 'line',
+        data: {
+            labels: labels(startDate, endDate, startMonth),
+            datasets: [{
+                label: `${DEVISE} : masquez moi`,
+                data: [rates[DEVISE].start_rate, rates[DEVISE].end_rate],
+                borderColor: 'red',
+                backgroundColor: '#f8c0b4'
+            }]
+        },
+        options: {
+            transitions: {
+                show: {
+                    animations: {
+                        x: {
+                            from: 0
+                        },
+                        y: {
+                            from: 0
+                        }
+                    }
+                },
+                hide: {
+                    animations: {
+                        x: {
+                            to: 0
+                        },
+                        y: {
+                            to: 0
+                        }
+                    }
+                }
+            }
+        }
+        })
+    })
+    .catch(error => console.log('error', error));
 })
--- JS/conversion.js
+++ JS/conversion.js
@@ -2,14 +2,6 @@
 let deviseInput  = document.getElementById('devise-input');
 let deviseChart  = document.getElementById('devise-chart');
 let deviseOutput = document.getElementById('devise-output');
-let myHeaders = new Headers();
-myHeaders.append("apikey", "g5abhnyuIjiV7oWQhZO6J7j7CJV3ac5h");
-
-let requestOptions = {
-  method: 'GET',
-  redirect: 'follow',
-  headers: myHeaders
-};
 
 fetch("https://api.apilayer.com/exchangerates_data/symbols", requestOptions)
   .then(response => response.json())
@@ -30,7 +22,9 @@ let priceOutput   = document.getElementById('price-output');
 convertButton.addEventListener('click', () => {
   let priceInput   = document.getElementById('price-input').value;
   let deviseInput  = document.getElementById('devise-input').value;
-  let deviseOutput = document.getElementById('devise-output').value; 
+  let deviseOutput = document.getElementById('devise-output').value;
+  let myHeaders = new Headers();
+  myHeaders.append("apikey", "g5abhnyuIjiV7oWQhZO6J7j7CJV3ac5h");
 
   myHeaders = new Headers();
   myHeaders.append("apikey", "g5abhnyuIjiV7oWQhZO6J7j7CJV3ac5h");
@@ -43,8 +37,7 @@ convertButton.addEventListener('click', () => {
 
   fetch(`https://api.apilayer.com/fixer/convert?to=${deviseOutput}&from=${deviseInput}&amount=${priceInput}`, requestOptions)
     .then(response => response.json())
-    .then(result =>
-      { console.log(result.message);
+    .then(result => {
         if (result.message.includes("You have exceeded") ){
           console.log("je suis dans la première condition");
           fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${deviseOutput}&from=${deviseInput}&amount=${priceInput}`, requestOptions)
