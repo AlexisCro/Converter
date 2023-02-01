76cbb31c4a79af3d4a22d2c12918c75ad1095bad
AlexisCro <95683483+AlexisCro@users.noreply.github.com>
2023-02-01 11:35:42 +0100

    add more data between two dates

--- JS/chartjs.js
+++ JS/chartjs.js
@@ -41,7 +41,6 @@ function labels(date1, date2, startMonth){
     let array = [];
     let m = startMonth;
     for (let i = 0; i <= numberOfMonthBetweenTwoDates(date1, date2); i++){
-        console.log(m);
         array.push(MONTHS[m]);
         m++
         if(m > 12){
@@ -52,6 +51,48 @@ function labels(date1, date2, startMonth){
     };
     return array;
 };
+
+function formatIntegerToString(integer){
+    let result = integer.toString();
+    if ( result.length == 1 ){
+        result = "0" + result;
+    }else{
+        result = result;
+    }
+    return result;
+}
+
+function dataAccordingToDate(dataRates, startDate, endDate, devise){
+    let array      = [];
+    let first      = startDate.substring(5, 7);
+    let firstMonth = parseInt(first);
+    let end        = endDate.substring(5, 6);
+    let startYear  = startDate.substring(0, 4);
+    let startDay   = startDate.substring(8, 10);
+    let date1      = new Date(startDate);
+    let date2      = new Date(endDate);
+    console.log(numberOfMonthBetweenTwoDates(date1, date2));
+    for (let i = 0; i <= numberOfMonthBetweenTwoDates(date1, date2); i++){
+        console.log(new Date(`${startYear}-${formatIntegerToString(firstMonth)}-${startDay}`) > date2);
+        if (new Date(`${startYear}-${formatIntegerToString(firstMonth)}-${startDay}`) > date2 == true){
+            console.log("je passe dans la condition undefined");
+            array.push(dataRates.rates[endDate][devise]);
+        }else{
+            array.push(dataRates.rates[`${startYear}-${formatIntegerToString(firstMonth)}-${startDay}`][devise]);
+            console.log(`date où ça merde : ${startYear}-${formatIntegerToString(firstMonth)}-${startDay}`);
+        }
+        firstMonth++;
+        if (firstMonth > 12 ){
+            firstMonth = 1;
+            startYear++;
+            startYear = startYear.toString();
+        };
+        console.log(`${startYear}-${formatIntegerToString(firstMonth)}-${startDay}`);
+        console.log("i : " + i);
+    }
+    console.log(array);
+    return array;
+}
 
 // display default chart
 const exampleChart = new Chart(myChart, {
@@ -102,7 +143,7 @@ BUTTON.addEventListener('click', ()=>{
     const endYear      = endDate.getFullYear();
     const DEVISE       = document.getElementById('devise-chart').value;
 
-    fetch(`https://api.apilayer.com/exchangerates_data/fluctuation?start_date=${startDateAPI}&end_date=${endDateAPI}&symbols=${DEVISE}`, requestOptions)
+    fetch(`https://api.apilayer.com/exchangerates_data/timeseries?start_date=${startDateAPI}&end_date=${endDateAPI}&symbols=${DEVISE}`, requestOptions)
     .then(response => response.json())
     .then(result => {
         let rates = result["rates"];
@@ -115,7 +156,7 @@ BUTTON.addEventListener('click', ()=>{
                 labels: labels(startDate, endDate, startMonth),
                 datasets: [{
                     label: `${DEVISE} : masquez moi`,
-                    data: [rates[DEVISE].start_rate, rates[DEVISE].end_rate],
+                    data: dataAccordingToDate(result, startDateAPI, endDateAPI, DEVISE),
                     borderColor: 'red',
                     backgroundColor: '#f8c0b4'
                 }]
@@ -152,7 +193,7 @@ BUTTON.addEventListener('click', ()=>{
                     labels: labels(startDate, endDate, startMonth),
                     datasets: [{
                         label: `${DEVISE} : masquez moi`,
-                        data: [rates[DEVISE].start_rate, rates[DEVISE].end_rate],
+                        data: dataAccordingToDate(result, startDateAPI, endDateAPI, DEVISE),
                         borderColor: 'red',
                         backgroundColor: '#f8c0b4'
                     }]
