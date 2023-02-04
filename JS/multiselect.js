54ff70d927fe3ca90d461d609f5d25452853ae3f
AlexisCro <95683483+AlexisCro@users.noreply.github.com>
2023-02-04 16:18:25 +0100

    try find a solution to remove active when we click out of the element

--- JS/multiselect.js
+++ JS/multiselect.js
@@ -1,9 +1,8 @@
 let select           = document.getElementById('visible-list-container');
 const LIST           = document.getElementById('hidden-list-container');
 const IMG            = document.getElementById('logo-expand');
-let inputMultiSelect = LIST.querySelectorAll('.input-multi-select');
 
-function activeMultiSelect(element){
+function toggleMultiSelect(element){
     element.addEventListener('click', ()=>{
         if ( !select.classList.contains('visible-list-container-active') && !LIST.classList.contains('hidden-list-container-visible')){
             select.classList.add('visible-list-container-active');
@@ -16,16 +15,30 @@ function activeMultiSelect(element){
     });
 }
 
-activeMultiSelect(IMG);
-activeMultiSelect(select);
-activeMultiSelect(IMG.parentNode);
+toggleMultiSelect(IMG);
+toggleMultiSelect(select);
+toggleMultiSelect(IMG.parentNode);
 
 document.addEventListener('click', (event)=>{
     console.log(event.target);
-    /*if ( event.target.parentNode.id != 'multi-select'){
+    /*if ( !event.target.className.includes('input-group-text') ||
+           !event.target.previousElementSibling.className.includes('input-multi-select') ||
+           !event.target.previousElementSibling.className.includes('input-group-text') ||
+           !event.target.className.includes('input-multi-select') ||
+           event.target.id != 'hidden-list-container' ||
+           !event.target.firstElementChild.className.includes('input-multi-select') ||
+           event.target.id != 'logo-expand-container' ||
+           event.target.id != 'logo-expand'
+    ){
         if (LIST.classList.contains('hidden-list-container-visible')){
             select.classList.remove('visible-list-container-active');
             LIST.classList.remove('hidden-list-container-visible');
         }
     }*/
 });
+
+// Si event.target.className.contains('input-group-text') => gère la div visible-list
+// Si event.target.previousElementSibling.className.conatins('input-multi-select') => gère le label des input
+// Si event.target.className.contains('input-multi-select') => gère les input
+// Si event.target.id === ''hidden-list-container' => gère la div de la liste de choix
+// Si event.target.firstElementChild.className.contains('input-multi-select') => gère les <li>
