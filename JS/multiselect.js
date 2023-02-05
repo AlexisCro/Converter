c5335b8524221c5bc17dc33f08296395dcef8d80
AlexisCro <95683483+AlexisCro@users.noreply.github.com>
2023-02-05 19:49:32 +0100

    add a recursive function to manage toggle and 'focus' on multiselect

--- JS/multiselect.js
+++ JS/multiselect.js
@@ -1,6 +1,7 @@
 let select           = document.getElementById('visible-list-container');
 const LIST           = document.getElementById('hidden-list-container');
 const IMG            = document.getElementById('logo-expand');
+const MAIN           = document.querySelector('main');
 
 function toggleMultiSelect(element){
     element.addEventListener('click', ()=>{
@@ -15,30 +16,26 @@ function toggleMultiSelect(element){
     });
 }
 
-toggleMultiSelect(IMG);
-toggleMultiSelect(select);
-toggleMultiSelect(IMG.parentNode);
-
-document.addEventListener('click', (event)=>{
-    console.log(event.target);
-    /*if ( !event.target.className.includes('input-group-text') ||
-           !event.target.previousElementSibling.className.includes('input-multi-select') ||
-           !event.target.previousElementSibling.className.includes('input-group-text') ||
-           !event.target.className.includes('input-multi-select') ||
-           event.target.id != 'hidden-list-container' ||
-           !event.target.firstElementChild.className.includes('input-multi-select') ||
-           event.target.id != 'logo-expand-container' ||
-           event.target.id != 'logo-expand'
-    ){
+function closeMultiSelect(element) {
+    console.log(element);
+    if (element.parentNode.id == 'multi-select'){
+        return false;
+    }else if (element.parentNode.id != 'multi-select' && element != MAIN){
+        element = element.parentNode;
+        closeMultiSelect(element);
+    }else if (element == MAIN){
         if (LIST.classList.contains('hidden-list-container-visible')){
             select.classList.remove('visible-list-container-active');
             LIST.classList.remove('hidden-list-container-visible');
         }
-    }*/
-});
+    }
+}
 
-// Si event.target.className.contains('input-group-text') => gère la div visible-list
-// Si event.target.previousElementSibling.className.conatins('input-multi-select') => gère le label des input
-// Si event.target.className.contains('input-multi-select') => gère les input
-// Si event.target.id === ''hidden-list-container' => gère la div de la liste de choix
-// Si event.target.firstElementChild.className.contains('input-multi-select') => gère les <li>
+toggleMultiSelect(IMG);
+toggleMultiSelect(select);
+toggleMultiSelect(IMG.parentNode);
+
+document.addEventListener('click', (click)=>{
+    target = click.target;
+    closeMultiSelect(target);
+})
