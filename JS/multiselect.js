dc8619d9495822c27ab4c492d58fdd1d85eb184e
AlexisCro <95683483+AlexisCro@users.noreply.github.com>
2023-02-06 19:38:26 +0100

    start function to construct and display choice list

--- JS/multiselect.js
+++ JS/multiselect.js
@@ -1,16 +1,17 @@
 let select           = document.getElementById('visible-list-container');
-const LIST           = document.getElementById('hidden-list-container');
-const IMG            = document.getElementById('logo-expand');
-const MAIN           = document.querySelector('main');
+const listContainer  = document.getElementById('hidden-list-container');
+const img            = document.getElementById('logo-expand');
+const main           = document.querySelector('main');
+let   choiceList     = document.getElementById('hidden-list').children;
 
-function toggleMultiSelect(element){
+function toggleMultiSelect(element) {
     element.addEventListener('click', ()=>{
-        if ( !select.classList.contains('visible-list-container-active') && !LIST.classList.contains('hidden-list-container-visible')){
+        if ( !select.classList.contains('visible-list-container-active') && !listContainer.classList.contains('hidden-list-container-visible')){
             select.classList.add('visible-list-container-active');
-            LIST.classList.add('hidden-list-container-visible');    
-        }else if (select.classList.contains('visible-list-container-active') && LIST.classList.contains('hidden-list-container-visible')){
+            listContainer.classList.add('hidden-list-container-visible');    
+        }else if (select.classList.contains('visible-list-container-active') && listContainer.classList.contains('hidden-list-container-visible')){
             select.classList.remove('visible-list-container-active');
-            LIST.classList.remove('hidden-list-container-visible');
+            listContainer.classList.remove('hidden-list-container-visible');
         }
         
     });
@@ -20,22 +21,41 @@ function closeMultiSelect(element) {
     console.log(element);
     if (element.parentNode.id == 'multi-select'){
         return false;
-    }else if (element.parentNode.id != 'multi-select' && element != MAIN){
+    }else if (element.parentNode.id != 'multi-select' && element != main){
         element = element.parentNode;
         closeMultiSelect(element);
-    }else if (element == MAIN){
-        if (LIST.classList.contains('hidden-list-container-visible')){
+    }else if (element == main){
+        if (listContainer.classList.contains('hidden-list-container-visible')){
             select.classList.remove('visible-list-container-active');
-            LIST.classList.remove('hidden-list-container-visible');
+            listContainer.classList.remove('hidden-list-container-visible');
         }
     }
 }
 
-toggleMultiSelect(IMG);
+function isChecked(elements) {
+    for(li in elements){
+        let child = elements[li];
+        if (child.tagName == "LI"){
+            child = child.children;
+            console.log(child);
+            for(input in child){
+                let checkbox = child[input];
+                if ( checkbox.type == "checkbox"){
+                    if ( checkbox.checked ){
+                        console.log(`${checkbox} is true`);
+                    }
+                }
+            }
+        }
+    }
+}
+
+toggleMultiSelect(img);
 toggleMultiSelect(select);
-toggleMultiSelect(IMG.parentNode);
+toggleMultiSelect(img.parentNode);
 
 document.addEventListener('click', (click)=>{
     target = click.target;
     closeMultiSelect(target);
+    isChecked(choiceList);
 })
